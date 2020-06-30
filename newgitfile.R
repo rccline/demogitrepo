ebe6500b6d5834068d6b750c9f11cfc972b8de2a
Robert <rccline@fastmail.fm>
2020-06-30 06:26:30 -0600

    Modified data_df
    Data_df has been modified

--- newgitfile.R
+++ newgitfile.R
@@ -1,3 +1,12 @@
 library(tidyverse)
 
-data_df <- c(1, 2, 4, 9)
+
+
+data_df <- tibble(y= rnorm(100),
+       x=runif(200))
+
+# 
+# From Mark Andrews to Everyone:  04:48 AM
+# https://help.github.com/en/github/using-git/associating-text-editors-with-git#using-atom-as-your-editor-2
+# From Mike Dunbar to Everyone:  04:49 AM
+# This is what I found (mentions mac) https://cityofaustin.github.io/ctm-dev-workflow/local-environment.html
